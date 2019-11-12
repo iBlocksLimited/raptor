74de274cf7902e63fc8d902c158357eb3bd35442
Connor Valerio <connor.valerio@iblocks.co.uk>
2019-11-12 11:28:13 +0000

    Added environment variables to configure logger + Added Console logger transport

--- src/logger.ts
+++ src/logger.ts
@@ -3,6 +3,11 @@ const winston = require("winston");
 // list of logging levels in order
 export const logLevels = ["error", "warn", "info", "verbose", "debug", "silly"];
 
+// read in environment variables
+const logLevel = process.env.RAPTOR_LOG_LEVEL && logLevels.includes(process.env.RAPTOR_LOG_LEVEL) ? process.env.RAPTOR_LOG_LEVEL : "info";
+const logFile = process.env.RAPTOR_LOG_LOCATION || "/var/log/raptor/raptor.log";
+const logToConsole = process.env.RAPTOR_LOG_TO_CONSOLE || false;
+
 const printOptions = {
     year: "numeric",
     month: "2-digit",
@@ -23,13 +28,19 @@ export const logger = winston.createLogger({
     format: logFormat,
     transports: [
         new winston.transports.File({
-            level: "info",
-            filename: `/var/log/raptor/raptor.log`,
+            level: logLevel,
+            filename: logFile,
             handleExceptions: true,
             json: true,
             maxsize: 52428800, // 50MB
             maxFiles: 5,
             colorize: false,
+        }),
+        new winston.transports.Console({
+            level: logLevel,
+            silent: !logToConsole,
+            handleExceptions: true,
+            json: true
         })
     ]
 });
